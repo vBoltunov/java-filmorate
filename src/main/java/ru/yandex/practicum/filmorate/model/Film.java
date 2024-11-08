acfbde91861ffde0a17e29aad42f13f2bdd811c1
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-11-08 16:13:47 +0300

    Add Film class fields

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,10 +3,17 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 /**
  * Film.
  */
 @Getter
 @Setter
 public class Film {
+    Long id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    long duration;
 }

270e7569f141695c33c893b6c8c806029e20b05c
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-11-08 17:16:44 +0300

    User: add validation to class fields

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.util.Date;
@@ -10,8 +14,16 @@ import java.util.Date;
 @Data
 public class User {
     Long id;
+
+    @Email(message = "Email is not valid")
     String email;
+
+    @NotBlank(message = "Login name must not be blank")
+    @Pattern(regexp = "\\S+", message = "Login name must not contain spaces")
     String login;
+
     String name;
+
+    @Past(message = "Birth date must not be in the future")
     Date birthday;
 }

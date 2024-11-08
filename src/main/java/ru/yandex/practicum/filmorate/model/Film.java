50099f636edae20240ce85ae2a997b16724acbc7
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-11-08 17:16:18 +0300

    Film: add validation to class fields

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,8 +14,16 @@ import java.time.LocalDate;
 @Data
 public class Film {
     Long id;
+
+    @NotBlank(message = "Film name must not be empty")
     String name;
+
+    @Size(max = 200, message = "Description is too long. Maximum length: 200")
     String description;
+
+    @NotNull(message = "Release date must not be null")
     LocalDate releaseDate;
+
+    @Positive(message = "Duration should be a positive number")
     long duration;
 }

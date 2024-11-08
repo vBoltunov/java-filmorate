3c42d8075ea93157e9fc69829fd0ed81c8a39ac3
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-11-08 17:02:52 +0300

    Add Slf4j logging to controller classes

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +16,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
@@ -23,41 +25,53 @@ public class FilmController {
 
     @GetMapping
     public Collection<Film> getFilms() {
+        log.info("Fetching all films.");
         return films.values();
     }
 
     @PostMapping
     public Film create(@RequestBody Film film) {
+        log.info("Creating film: {}", film);
+
         if (film.getName() == null || film.getName().isBlank()) {
+            log.warn("Validation failed: Film name must not be empty.");
             throw new ValidationException("Название фильма не может быть пустым");
         }
 
         if (film.getDescription().length() > 200) {
+            log.warn("Validation failed: Description is too long. Maximum length: 200.");
             throw new ValidationException("Максимальная длина описания - 200 символов");
         }
 
         if (film.getReleaseDate().isBefore(MIN_RELEASE_DATE)) {
+            log.warn("Validation failed: Film release date is before the minimum allowed date.");
             throw new ValidationException("Дата релиза не может быть раньше 28.12.1895");
         }
 
         if (film.getDuration() < 0) {
+            log.warn("Validation failed: Duration should be a positive number.");
             throw new ValidationException("Продолжительность фильма должна быть положительным числом");
         }
 
         film.setId(getNextId());
         films.put(film.getId(), film);
+        log.info("Film created successfully: {}", film);
 
         return film;
     }
 
     @PutMapping
     public Film update(@RequestBody Film newFilm) {
+        log.info("Updating film with id: {}", newFilm.getId());
+
         if (newFilm.getId() == null) {
+            log.warn("Validation failed: Film ID must be provided.");
             throw new ValidationException("Id должен быть указан");
         }
 
         Film oldFilm = films.get(newFilm.getId());
         if (oldFilm == null) {
+            log.error("Film not found: id = {}", newFilm.getId());
             throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
         }
 
@@ -73,6 +87,7 @@ public class FilmController {
         if (newFilm.getDuration() > 0) {
             oldFilm.setDuration(newFilm.getDuration());
         }
+        log.info("Film updated successfully: {}", oldFilm);
 
         return oldFilm;
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -24,57 +26,73 @@ public class UserController {
 
     @GetMapping
     public Collection<User> getUsers() {
+        log.info("Fetching all users.");
         return users.values();
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
+        log.info("Creating user: {}", user);
+
         if (user.getEmail() == null || user.getEmail().isBlank()) {
+            log.warn("Validation failed: Email must be provided.");
             throw new ValidationException("Эл. почта должна быть указана");
         }
 
         if (!user.getEmail().contains("@")) {
+            log.warn("Validation failed: Email is not valid.");
             throw new ValidationException("Некорректная эл. почта");
         }
 
         if (user.getLogin() == null || user.getLogin().isBlank()) {
+            log.warn("Validation failed: Login name must not be blank.");
             throw new ValidationException("Логин должен быть указан");
         }
 
         if (user.getLogin().contains(" ")) {
+            log.warn("Validation failed: Login name must not contain whitespaces.");
             throw new ValidationException("Логин не должен содержать пробелы");
         }
 
         if (user.getName() == null || user.getName().isBlank()) {
+            log.info("Name not provided. Using email instead of name.");
             user.setName(user.getEmail());
         }
 
         if (user.getBirthday().after(Date.from(Instant.now()))) {
+            log.warn("Validation failed: Birth date must not be in the future.");
             throw new ValidationException("Дата рождения не может быть в будущем");
         }
 
         user.setId(getNextId());
         users.put(user.getId(), user);
+        log.info("User created successfully: {}", user);
 
         return user;
     }
 
     @PutMapping
     public User update(@RequestBody User newUser) {
+        log.info("Updating user with id: {}", newUser.getId());
+
         if (newUser.getId() == null) {
+            log.warn("Validation failed: ID must be provided.");
             throw new ValidationException("Id должен быть указан");
         }
         if (users.containsKey(newUser.getId())) {
             User oldUser = users.get(newUser.getId());
             for(User savedUser : users.values()) {
                 if (Objects.equals(savedUser.getEmail(), newUser.getEmail())) {
+                    log.warn("Validation failed: Email is already in use.");
                     throw new ValidationException("Эта эл. почта уже используется");
                 }
             }
             oldUser.setEmail(newUser.getEmail());
+            log.info("User updated successfully: {}", oldUser);
 
             return oldUser;
         }
+        log.error("User not found: id = {}", newUser.getId());
         throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
     }
 

d99e7936e9f7bb24b9e37f21f97f444933033297
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-11-23 23:49:24 +0300

    FilmStorage: add methods

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,4 +1,18 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.Optional;
+
 public interface FilmStorage {
+
+    Collection<Film> getFilms();
+
+    Film createFilm(Film film);
+
+    Film updateFilm(Film film);
+
+    Optional<Film> getFilmById(Long id);
+
 }

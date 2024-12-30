b45a56538071c5b9819b7cf68ee07e5988a59075
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-12-30 11:48:27 +0300

    Add FriendDto

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/user/FriendDto.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FriendDto {
+    private Long id;
+    private String status;
+}

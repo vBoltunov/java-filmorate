ef7f932e230e2f32fbc45115e1c5d196c74a82bc
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-12-30 11:52:40 +0300

    Add UpdateFilmRequest dto

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/film/requests/UpdateFilmRequest.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.filmorate.dto.film.requests;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class UpdateFilmRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Long duration;
+    private Long mpaId;
+    private List<Long> genreIds;
+    private List<Long> likes;
+
+    public boolean hasName() {
+        return ! (name == null || name.isBlank());
+    }
+    public boolean hasDescription() {
+        return ! (description == null || description.isBlank());
+    }
+    public boolean hasReleaseDate() {
+        return releaseDate != null;
+    }
+    public boolean hasDuration() {
+        return duration != null;
+    }
+    public boolean hasMpaId() {
+        return mpaId != null;
+    }
+    public boolean hasGenreIds() {
+        return ! (genreIds == null || genreIds.isEmpty());
+    }
+}

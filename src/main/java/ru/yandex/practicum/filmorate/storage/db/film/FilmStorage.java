d96feed47e924e0b8f1c28fb1229b62680170c37
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-12-30 12:07:39 +0300

    FilmStorage interface: add methods

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/film/FilmStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.storage.db.film;
 import ru.yandex.practicum.filmorate.model.film.Film;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
@@ -13,6 +14,14 @@ public interface FilmStorage {
 
     Film updateFilm(Film film);
 
+    void updateFilmGenres(Film film);
+
     Optional<Film> getFilmById(Long id);
+
+    void addLike(Long filmId, Long userId);
+
+    void removeLike(Long filmId, Long userId);
+
+    List<Film> getPopularFilms(int count);
 
 }

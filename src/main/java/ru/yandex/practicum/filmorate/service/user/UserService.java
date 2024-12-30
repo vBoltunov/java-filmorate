380e7138a2a3b59ccd1786c5b0d494b3e30477fc
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-12-30 12:24:49 +0300

    UserService: change logic

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -1,16 +1,23 @@
 package ru.yandex.practicum.filmorate.service.user;
 
+import jakarta.validation.ConstraintViolation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dto.user.UserDto;
+import ru.yandex.practicum.filmorate.dto.user.requests.NewUserRequest;
+import ru.yandex.practicum.filmorate.dto.user.requests.UpdateUserRequest;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.mapper.user.UserMapper;
 import ru.yandex.practicum.filmorate.model.user.User;
 import ru.yandex.practicum.filmorate.storage.db.user.UserStorage;
+import ru.yandex.practicum.filmorate.util.FormatUtil;
+import ru.yandex.practicum.filmorate.util.ValidationUtil;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -19,79 +26,57 @@ import java.util.Set;
 public class UserService {
     private final UserStorage userStorage;
 
-    private void validateUsersExist(Long userId, Long friendId) {
-        getUserByIdOrThrow(userId);
-        getUserByIdOrThrow(friendId);
-    }
+    public UserDto createUser(NewUserRequest request) {
+        try {
+            Optional<User> alreadyExistUser = userStorage.findByEmail(request.getEmail());
+            if (alreadyExistUser.isPresent()) {
+                throw new DuplicatedDataException("Email already in use");
+            }
 
-    private User getUserByIdOrThrow(Long userId) {
-        return userStorage.getUserById(userId).orElseThrow(() ->
-                new NotFoundException(String.format("User with id %s not found", userId)));
-    }
+            User user = UserMapper.mapToUser(request);
 
-    private void initializeFriends(User user) {
-        if (user.getFriends() == null) {
-            user.setFriends(new HashSet<>());
+            Set<ConstraintViolation<User>> violations = ValidationUtil.validate(user);
+            if (!violations.isEmpty()) {
+                throw new ConditionsNotMetException(ValidationUtil.getFirstViolationMessage(violations));
+            }
+
+            user = userStorage.createUser(user);
+            return UserMapper.mapToUserDto(user);
+        } catch (Exception e) {
+            log.error("Error creating user: {}", e.getMessage());
+            throw e;
         }
     }
 
-    public void addFriend(Long userId, Long friendId) {
-        log.debug("Adding friend: userId={}, friendId={}", userId, friendId);
-        validateUsersExist(userId, friendId);
-
-        User user = getUserByIdOrThrow(userId);
-        User userFriend = getUserByIdOrThrow(friendId);
-
-        initializeFriends(user);
-        initializeFriends(userFriend);
-
-        user.getFriends().add(friendId);
-        userFriend.getFriends().add(userId);
-
-        userStorage.updateUser(user);
-        userStorage.updateUser(userFriend);
-
-        log.debug("Friend added successfully: user={}, friend={}", user, userFriend);
+    public UserDto getUserById(long userId) {
+        return userStorage.getUserById(userId)
+                .map(UserMapper::mapToUserDto)
+                .orElseThrow(() -> new NotFoundException(
+                        FormatUtil.formatNotFoundMessage("User", userId)));
     }
 
-    public void removeFriend(Long userId, Long friendId) {
-        validateUsersExist(userId, friendId);
-
-        User user = getUserByIdOrThrow(userId);
-        User userFriend = getUserByIdOrThrow(friendId);
-
-        initializeFriends(user);
-        initializeFriends(userFriend);
-
-        user.getFriends().remove(friendId);
-        userFriend.getFriends().remove(userId);
-
-        userStorage.updateUser(user);
-        userStorage.updateUser(userFriend);
-    }
-
-    public List<Map<String, Long>> getFriends(Long userId) {
-        User user = getUserByIdOrThrow(userId);
-        initializeFriends(user);
-        return user.getFriends().stream()
-                .map(friendId -> Collections.singletonMap("id", friendId))
+    public List<UserDto> getUsers() {
+        return userStorage.getUsers()
+                .stream()
+                .map(UserMapper::mapToUserDto)
                 .toList();
     }
 
-    public List<Map<String, Long>> getCommonFriends(Long userId, Long otherUserId) {
-        validateUsersExist(userId, otherUserId);
-
-        User user = getUserByIdOrThrow(userId);
-        User otherUser = getUserByIdOrThrow(otherUserId);
-
-        initializeFriends(user);
-        initializeFriends(otherUser);
-
-        Set<Long> commonFriends = new HashSet<>(user.getFriends());
-        commonFriends.retainAll(otherUser.getFriends());
-
-        return commonFriends.stream()
-                .map(friendId -> Collections.singletonMap("id", friendId))
-                .toList();
+    public UserDto updateUser(long userId, UpdateUserRequest request) {
+        try {
+            User updatedUser = userStorage.getUserById(userId)
+                    .map(user -> UserMapper.updateUserFields(user, request))
+                    .orElseThrow(() -> new NotFoundException(
+                            FormatUtil.formatNotFoundMessage("User", userId)));
+            updatedUser = userStorage.updateUser(updatedUser);
+            return UserMapper.mapToUserDto(updatedUser);
+        } catch (NotFoundException e) {
+            log.error("User not found: {}", e.getMessage());
+            throw new NotFoundException(
+                    FormatUtil.formatNotFoundMessage("User", userId));
+        } catch (Exception e) {
+            log.error("Error updating user: {}", e.getMessage());
+            throw e;
+        }
     }
 }

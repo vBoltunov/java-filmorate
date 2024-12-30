f8bf2b0c2dd8619d194b0a8a669f8c297c46dcaa
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-12-30 11:47:37 +0300

    Add MpaDto

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/film/MpaDto.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.dto.film;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MpaDto {
+    private Long id;
+    private String name;
+}

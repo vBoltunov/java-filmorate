1f0204ffce1bf651f59fd93461ee35ac522e0210
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-01-02 21:20:51 +0300

    UserRowMapper: map to 'friends_ids' column

--- src/main/java/ru/yandex/practicum/filmorate/storage/mappers/user/UserRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mappers/user/UserRowMapper.java
@@ -6,6 +6,9 @@ import ru.yandex.practicum.filmorate.model.user.User;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 public class UserRowMapper implements RowMapper<User> {
@@ -17,6 +20,17 @@ public class UserRowMapper implements RowMapper<User> {
         user.setLogin(resultSet.getString("login"));
         user.setName(resultSet.getString("name"));
         user.setBirthday(resultSet.getDate("birthday").toLocalDate());
+
+        String friendsIds = resultSet.getString("friends_ids");
+
+        if (friendsIds != null && !friendsIds.isEmpty()) {
+            List<Long> friendsList = Arrays.stream(friendsIds.split(","))
+                    .map(Long::valueOf)
+                    .toList();
+            user.setFriendsIds(friendsList);
+        } else {
+            user.setFriendsIds(Collections.emptyList());
+        }
 
         return user;
     }
